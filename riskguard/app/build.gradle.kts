3cb557935b2ccd478d988596edc14c23d9dfa7e1
vitor <projetointegradorpuc10@gmail.com>
2025-06-05 20:26:24 -0300

    ajuste json

--- riskguard/app/build.gradle.kts
+++ riskguard/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.riskguard.appadmin"
         minSdk = 23
         targetSdk = 35
         versionCode = 1
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity:1.10.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
